785b4723ecd8d80bfce008a73c323409f6da1b68
Vishakha Kalra <vishakha.kalra@impressico.com>
2024-06-20 10:42:13 +0530

    template updated

--- vars/mygroovy.groovy
+++ vars/mygroovy.groovy
@@ -1,7 +1,6 @@
 // vars/deployCloudFormation.groovy
 
 def call(String templateFile, String stackName, Map<String, String> parameters) {
-    // AWS CLI command components
     def awsCliCmd = "aws"
     def awsCliArgs = [
         "cloudformation",
@@ -9,20 +8,12 @@ def call(String templateFile, String stackName, Map<String, String> parameters) 
         "--template-file", templateFile,
         "--stack-name", stackName
     ]
-
-    // Adding parameter overrides
     parameters.each { key, value ->
         awsCliArgs += "--parameter-overrides", "${key}=${value}"
     }
-
-    // Constructing the command string
     def command = [awsCliCmd] + awsCliArgs
-
-    // Execute the command
     def proc = command.execute()
     proc.waitFor()
-
-    // Handling command execution result
     if (proc.exitValue() != 0) {
         error "Failed to deploy CloudFormation stack. Exit code: ${proc.exitValue()}"
     } else {
