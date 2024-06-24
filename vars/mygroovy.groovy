acae7e51166fbff13572a6d4924efc767455d6e1
Vishakha Kalra <vishakha.kalra@impressico.com>
2024-06-24 14:22:46 +0530

    mygroovy again

--- vars/mygroovy.groovy
+++ vars/mygroovy.groovy
@@ -27,27 +27,3 @@ def deploy(String templateFile, String stackName, Map<String, String> parameters
     }
 }
 
-def deploy(String templateFile, String stackName, Map<String, String> parameters) {
-    def awsCliCmd = "aws"
-    def awsCliArgs = [
-        "cloudformation",
-        "deploy",
-        "--template-file", templateFile,
-        "--stack-name", stackName
-    ]
-
-    parameters.each { key, value ->
-        awsCliArgs.addAll(["--parameter-overrides", "${key}=${value}"])
-    }
-
-    def command = [awsCliCmd] + awsCliArgs
-    def proc = command.execute()
-    proc.waitFor()
-
-    if (proc.exitValue() != 0) {
-        error "Failed to deploy CloudFormation stack. Exit code: ${proc.exitValue()}"
-    } else {
-        println "CloudFormation stack deployed successfully"
-    }
-}
-
