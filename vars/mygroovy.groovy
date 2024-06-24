10b5c7f3d663a6e52428b346506b5dda6cc5e77b
Vishakha Kalra <vishakha.kalra@impressico.com>
2024-06-24 14:13:57 +0530

    passing parameters

--- vars/mygroovy.groovy
+++ vars/mygroovy.groovy
@@ -1,7 +1,6 @@
 // vars/mygroovy.groovy
-def call() {
+def call(String stackName) {
     def templateFile = '/tmp/website2.yaml'
-    def stackName = 'stacknew'
     def parameters = [
         'BucketName': 'vishcfnhost5'
     ]
@@ -9,6 +8,25 @@ def call() {
     // Execute CloudFormation deployment
     deploy(templateFile, stackName, parameters)
 }
+
+def deploy(String templateFile, String stackName, Map<String, String> parameters) {
+    def awsCliCmd = "aws cloudformation deploy"
+    def parameterOverrides = parameters.collect { key, value -> "--parameter-overrides ${key}=${value}" }.join(" ")
+
+    // Construct the full command
+    def command = "${awsCliCmd} --template-file ${templateFile} --stack-name ${stackName} ${parameterOverrides}"
+
+    // Execute the command
+    def proc = command.execute()
+    proc.waitFor()
+
+    if (proc.exitValue() != 0) {
+        error "Failed to deploy CloudFormation stack. Exit code: ${proc.exitValue()}"
+    } else {
+        echo "CloudFormation stack deployed successfully"
+    }
+}
+
 def deploy(String templateFile, String stackName, Map<String, String> parameters) {
     def awsCliCmd = "aws"
     def awsCliArgs = [
